0f82aa6199528698ee5178363ca8e10f46ecc193
Troels Bjørnskov <troels@bjoernskov.org>
2021-03-25 18:25:38 +0100

    Further clarify install process

--- zsh-file-opener.zsh
+++ zsh-file-opener.zsh
@@ -7,7 +7,9 @@ if [[ $SSH_TTY ]]; then
         print -P "%F{5}Installing %F{33}rmate%F{5} helper for Sublime Text…%f"
         curl --silent -o $HOME/.local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate &&\
         chmod +x $HOME/.local/bin/rmate &&\
-        print -P "%F{2}%{\e[3m%}rmate Installed.%f%b"
+        print -P "%F{2}%{\e[3m%}rmate Installed.%f%b" ||\
+        print -P "%F{2}%{\e[3m%}Could not install rmate.%f%b"
+
     fi
 
     _file_opener() {
