f294d04d6f729d0a693d6a151e6c224acaf6e5a3
Troels Bjørnskov <troels@bjoernskov.org>
2021-03-26 19:45:55 +0100

    Ensure directory is created

--- zsh-file-opener.zsh
+++ zsh-file-opener.zsh
@@ -3,6 +3,7 @@ alias ${_ZSH_FILE_OPENER_CMD:-u}='_file_opener'
 if [[ $SSH_TTY ]]; then
     if ! command -v rmate &> /dev/null; then
         print -P "%F{5}Installing %F{33}rmate%F{5} helper for Sublime Text…%f"
+        command mkdir -p "${HOME}/.local/bin"
         curl --silent -o $HOME/.local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate &&\
         chmod +x $HOME/.local/bin/rmate &&\
         print -P "%F{2}%{\e[3m%}rmate Installed.%f%b" ||\
