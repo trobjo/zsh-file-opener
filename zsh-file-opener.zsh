trobjo/zsh-file-opener

commit 35d1c69b64defae80c1a4a180b2707c34288b67d

Fix duplicate name bug
This commit fixes a bug where _file_opener should refuse to open a file.

In the case where the file hierarchy was 0 levels deep (i.e. the file we
wanted to open was the same as pwd, the string would split incorrectly
due to no slashes being part of the string. Now, the string splits
correctly.

Also, the error message in the second case has been set to indicate a
deliberate action.

zsh-file-opener.zsh
@@ -10,7 +10,7 @@ _file_opener() {
     [[ -z "$@" ]] && cd "-" > /dev/null 2>&1 && return 0
     [[ -e "$1" ]] && [[ ! -r "$1" ]] && {
         [[ -d "$1" ]] && print "Permission denied: \x1B[3m\x1B[34m$1\033[0m" && return 1
-        [[ -f "$1" ]] && print "Permission denied: \x1B[3m\x1B[34m${1%/*}/\033[0m${1##*/}" && return 1
+        [[ -f "$1" ]] && print "Permission denied: \x1B[3m\x1B[34m${${1%*}%"${1##*/}"}\033[0m${1##*/}" && return 1
     }
     [[ -d "$@" ]] && cd "${@}" > /dev/null 2>&1 && return 0
 
@@ -27,8 +27,8 @@ _file_opener() {
                 swaymsg -q "[app_id=\"^org.pwmt.zathura$\" title=\"^${(q)file##*/}\ \[\"] focus" || pdfs+=("${file:a:q}") ;;
             (jpeg|jpg|png|webp|svg|gif|bmp|tif|tiff|psd)
                 pics+=("${file:a:q}") ;;
-                print "Cannot open \x1B[36m${file##*/}\033[0m" && local ret=1 ;;
             (${~_ZSH_FILE_OPENER_EXCLUDE_SUFFIXES//,/|})
+                print "File opener is disabled for: \x1B[3m\x1B[34m${${1%*}%"${1##*/}"}\033[0m${1##*/}" && local ret=1 ;;
             (html|mhtml)
                 urls+=("${file:a:q}") ;;
             (*)

commit 9f4867fba3b689573178e8ab0459b2f812d8fadd

Fix bug where wrong filename would be printed
This commit fixes a bug introduced in the previous commit (35d1c69b64defae80c1a4a180b2707c34288b67d)
where the first arg in the argslist, $@, was read and printed instead of
the one we have in our for loop, which is named $file.

zsh-file-opener.zsh
@@ -28,7 +28,7 @@ _file_opener() {
             (jpeg|jpg|png|webp|svg|gif|bmp|tif|tiff|psd)
                 pics+=("${file:a:q}") ;;
             (${~_ZSH_FILE_OPENER_EXCLUDE_SUFFIXES//,/|})
-                print "File opener is disabled for: \x1B[3m\x1B[34m${${1%*}%"${1##*/}"}\033[0m${1##*/}" && local ret=1 ;;
+                print "File opener is disabled for: \x1B[3m\x1B[34m${${file}%"${file##*/}"}\033[0m${file##*/}" && local ret=1 ;;
             (html|mhtml)
                 urls+=("${file:a:q}") ;;
             (*)